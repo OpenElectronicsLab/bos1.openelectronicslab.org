ae673379d88af349b14997cdad57526107708e99
Kendrick Shaw <kms15@case.edu>
2021-07-01 19:40:48 +0000

    spaces to tabs

--- secrets/generate-ansible-secret.sh
+++ secrets/generate-ansible-secret.sh
@@ -4,6 +4,6 @@
 # math.log(52**45)/math.log(2)
 # 256.51978731634915
 cat /dev/random \
-	| tr --delete --complement 'a-zA-Z' \
-	| head --bytes=45 \
-	| ansible-vault encrypt_string --stdin-name 'my_variable'
+    | tr --delete --complement 'a-zA-Z' \
+    | head --bytes=45 \
+    | ansible-vault encrypt_string --stdin-name 'my_variable'
